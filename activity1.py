10c25feec9733b68b10dda6f054f752418924776
POTATOPROGAMMER <codercode1010@gmail.com>
2024-12-31 16:51:46 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter the first number:" ))
+num2 = int(input("enter the second number:" ))
+num3 = int(input("enter the third number:" ))
+print("before swapping:" )
+print(num1, num2, num3)
+print("after swapping:" )
+print(num2,num3,num1)
